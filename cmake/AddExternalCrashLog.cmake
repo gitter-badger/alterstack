f6c97df027b9bbd4801e27b117c97c2981b29ce3
Alexey Syrnikov <san@masterspline.net>
2015-08-26 23:03:58 +0300

    updated crash_log download URL

--- cmake/AddExternalCrashLog.cmake
+++ cmake/AddExternalCrashLog.cmake
@@ -1,6 +1,6 @@
 ExternalProject_Add(EXT_CRASH_LOG
   #GIT_REPOSITORY /home/san/masterspline/Catch/
-  GIT_REPOSITORY https://github.com/masterspline/crashlog.git
+  GIT_REPOSITORY https://github.com/masterspline/crash_log.git
   GIT_TAG master # use branch name, tag or commit hash
   UPDATE_COMMAND ""
   #PATCH_COMMAND patch -p1 -t -N < ${CMAKE_SOURCE_DIR}/cmake/patch/Catch.patch
@@ -9,5 +9,3 @@ ExternalProject_Add(EXT_CRASH_LOG
   #BUILD_COMMAND ""
   #INSTALL_COMMAND ""
 )
-#ExternalProject_Get_Property(EXT_CPU_UTILS SOURCE_DIR)
-#include_directories(${CMAKE_BINARY_DIR}/InstalledExternals/include)
