2a4e3399c5a3fd41a32141aa067f8f315271d90f
Alexey Syrnikov <san@masterspline.net>
2015-09-13 21:40:32 +0300

    update boost to v1.59.0 in AddExternalBoost.cmake

--- cmake/AddExternalBoost.cmake
+++ cmake/AddExternalBoost.cmake
@@ -1,7 +1,9 @@
 if(CMAKE_SYSTEM_NAME MATCHES "Windows")
     set(EXT_BOOST_DOWNLOAD_URL "http://sourceforge.net/projects/boost/files/boost/1.59.0/boost_1_59_0.zip")
+    set(EXT_BOOST_URL_HASH "08d29a2d85db3ebc8c6fdfa3a1f2b83c")
 else()
-    set(EXT_BOOST_DOWNLOAD_URL "http://sourceforge.net/projects/boost/files/boost/1.58.0/boost_1_58_0.tar.bz2")
+    set(EXT_BOOST_DOWNLOAD_URL "http://sourceforge.net/projects/boost/files/boost/1.59.0/boost_1_59_0.tar.bz2")
+    set(EXT_BOOST_URL_HASH "6aa9a5c6a4ca1016edd0ed1178e3cb87")
 endif()
 #Bootstrap
 #if((CMAKE_SYSTEM_NAME MATCHES "Windows") AND (NOT CMAKE_CROSSCOMPILING))
@@ -15,6 +17,7 @@ ExternalProject_Add(EXT_BOOST
   #GIT_REPOSITORY https://github.com/boostorg/boost.git
   #GIT_TAG boost-1.59.0 # use branch name, tag or commit hash
   URL "${EXT_BOOST_DOWNLOAD_URL}"
+  URL_HASH MD5=${EXT_BOOST_URL_HASH}
   UPDATE_COMMAND ""
   CONFIGURE_COMMAND ./bootstrap.sh --with-libraries=context --prefix=${CMAKE_BINARY_DIR}/InstalledExternals
   BUILD_IN_SOURCE 1
@@ -23,5 +26,3 @@ ExternalProject_Add(EXT_BOOST
 )
 #ExternalProject_Get_Property(EXT_BOOST INSTALL_DIR)
 set(BOOST_ROOT "${CMAKE_BINARY_DIR}/InstalledExternals")
-
-#find_package(Boost 1.58.0 COMPONENTS context)
